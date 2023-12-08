d6dfac8bf9ca4de3658b5c82649d938aa4e694fb
antonygatua <antonnymuiko@gmail.com>
2023-12-09 02:47:16 +0300

    ByteShortIntLong data types

---
+++ ByteShortIntLong/src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+fun main() {
+    var number: Int = 25
+
+    val maxIntegerValue: Int = Int.MAX_VALUE
+    val minIntegerValue: Int = Int.MIN_VALUE
+
+    println("The maximum integer values is $maxIntegerValue")
+    println("The minimum integer values is $minIntegerValue")
+
+    val myMaxByteValue: Byte = Byte.MAX_VALUE
+    val myMinByteValue: Byte = Byte.MIN_VALUE
+
+    println("The maximum byte value is $myMaxByteValue")
+    println("The minimum byte value is $myMinByteValue")
+
+    val myMaxShortValue: Short = Short.MAX_VALUE
+    val myMinShortValue: Short = Short.MIN_VALUE
+
+    println("Maximum short Value $myMaxShortValue")
+    println("Minimum short Value $myMinShortValue")
+
+    val myMaxLongValue: Long = Long.MAX_VALUE
+    val myMinLongValue: Long = Long.MIN_VALUE
+
+    println("Maximum short Value $myMaxLongValue")
+    println("Minimum short Value $myMinLongValue")
+
+    var myNumber = 28 // default inferred as an integer, to change you have to specify the explicit type
+}
