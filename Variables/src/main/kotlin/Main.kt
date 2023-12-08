1c5a8b58ace1d39314d423afaaa04b53b8ef727d
antonygatua <antonnymuiko@gmail.com>
2023-12-09 01:41:25 +0300

    Getting started with Variables

---
+++ Variables/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var userName: String = "Antonny Muiko"
+    userName = "Muiko"
+    val age: Int = 25 // val to specify variable that never changes
+    println("Hello World, $userName has started learning Kotlin!!")
+    println("$userName started learning Kotlin at $age")
+
+}
